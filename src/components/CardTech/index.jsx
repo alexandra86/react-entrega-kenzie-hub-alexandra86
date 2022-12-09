59a1fa888c12bebcc020e9aa9bf35d8c5229bd5d
Alexandra Miranda <alexyamiranda7@gmail.com>
2022-12-09 12:40:31 -0300

    fix/ remoção de console.log na aplicação

--- src/components/CardTech/index.jsx
+++ src/components/CardTech/index.jsx
@@ -1,7 +1,6 @@
 import { StyleCardTech } from "./styles.js";
 
 export function CardTech({ elem, index }) {
-  console.log(elem);
   return (
     <StyleCardTech key={index}>
       <h3 className="technology">{elem.title}</h3>
